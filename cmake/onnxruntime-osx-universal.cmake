97c003f3bab12f8436e7f996738a55a11dba0685
nagae-memooff <169192@gmail.com>
2025-10-11 10:55:17 +0800

    升级onnxruntime,更改默认的聚类算法

--- cmake/onnxruntime-osx-universal.cmake
+++ cmake/onnxruntime-osx-universal.cmake
@@ -13,9 +13,9 @@ if(NOT BUILD_SHARED_LIBS)
   message(FATAL_ERROR "This file is for building shared libraries. BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 endif()
 
-set(onnxruntime_URL  "https://github.com/microsoft/onnxruntime/releases/download/v1.17.1/onnxruntime-osx-universal2-1.17.1.tgz")
-set(onnxruntime_URL2 "https://hf-mirror.com/csukuangfj/onnxruntime-libs/resolve/main/onnxruntime-osx-universal2-1.17.1.tgz")
-set(onnxruntime_HASH "SHA256=9fa57fa6f202a373599377ef75064ae568fda8da838632b26a86024c7378d306")
+# set(onnxruntime_URL  "https://github.com/microsoft/onnxruntime/releases/download/v1.17.1/onnxruntime-osx-universal2-1.17.1.tgz")
+# set(onnxruntime_URL2 "https://hf-mirror.com/csukuangfj/onnxruntime-libs/resolve/main/onnxruntime-osx-universal2-1.17.1.tgz")
+# set(onnxruntime_HASH "SHA256=9fa57fa6f202a373599377ef75064ae568fda8da838632b26a86024c7378d306")
 
 # If you don't have access to the Internet,
 # please download onnxruntime to one of the following locations.
@@ -26,6 +26,11 @@ set(possible_file_locations
   ${CMAKE_BINARY_DIR}/onnxruntime-osx-universal2-1.17.1.tgz
   /tmp/onnxruntime-osx-universal2-1.17.1.tgz
 )
+
+set(onnxruntime_URL  "https://github.com/microsoft/onnxruntime/releases/download/v1.23.1/onnxruntime-osx-universal2-1.23.1.tgz")
+set(onnxruntime_URL2 "https://github.com/microsoft/onnxruntime/releases/download/v1.23.1/onnxruntime-osx-universal2-1.23.1.tgz")
+set(onnxruntime_HASH "SHA256=e9fcd200bcf69377cc2c15e529368ca840d625669f34b1cf296a56b175665fc2")
+
 
 foreach(f IN LISTS possible_file_locations)
   if(EXISTS ${f})
